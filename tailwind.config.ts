4017577ebf08ae432b69ec2d4e480dccdd9ee9e1
Jokke Ruokolainen <jokke.ruokolainen@solita.fi>
2025-06-16 08:42:46 +0300

    fix: update border styling in Markdown components and MermaidDiagram

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,7 +7,11 @@ export default {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        border: "hsl(var(--border))",
+      },
+    },
   },
   plugins: [require("tailwindcss-animate")],
 } satisfies Config
